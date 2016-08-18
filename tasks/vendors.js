9a4af2d4bcbe7ee3ca862a34a45fcb96e2b30813
Daniel Hirunrusme <daniel@halfhalf.us>
2016-08-18 15:57:58 -0400

    init Shopify Theme

---
+++ tasks/vendors.js
@@ -0,0 +1,22 @@
+var gulp = require( "gulp" );
+var concat = require( "gulp-concat" );
+var uglify = require( "gulp-uglify" );
+var sourcemaps = require( "gulp-sourcemaps" );
+var vendorScripts = [
+];
+
+
+var vendorScripts = [
+
+];
+
+
+gulp.task( "vendors", function() {
+	return gulp.src( vendorScripts )
+    .pipe( sourcemaps.init() )
+    .pipe( concat( "vendor.min.js" ) )
+    .pipe( uglify() )
+    .pipe( sourcemaps.write() )
+    .pipe( gulp.dest( "assets/" ) );
+});
+ 
